b6e653eeb0176c26a2a2edc7796939a9a57fc2ec
ghost <ghost@void.com>
2020-05-02 12:39:09 +0800

    add stringptr and baseurl util

--- util.go
+++ util.go
@@ -2,9 +2,23 @@ package pie
 
 import (
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
 func init() {
 	rand.Seed(time.Now().Unix())
+}
+
+func StringPtr(s string) *string {
+	return &s
+}
+
+func RestBaseUrl() string {
+	if strings.ToLower(os.Getenv("APP_TLS_ENABLED")) == "true" {
+		return "https://" + strings.ToLower(os.Getenv("APP_REST_HOST"))
+	} else {
+		return "http://" + strings.ToLower(os.Getenv("APP_REST_HOST"))
+	}
 }
